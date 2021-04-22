d8f1d1837839f7dfafd11fbe2196437e00cdc109
Leo <mail@liuzheng.org>
2021-04-22 14:34:57 +0800

    加入方向控制和主框架基本完成

--- src/FinalProject/main.java
+++ src/FinalProject/main.java
@@ -1,5 +1,101 @@
 package FinalProject;
 
-public class main {
+import java.util.Scanner;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
+public class main {
+	
+	 private static boolean youWin = false;
+	 static int maploadDone = 0;
+	 static int xReal=0;
+	 static int yReal=0;
+	 
+	public static void main (String[] args) {
+		xReal=3;
+		yReal=3;
+	    JFrame f = new JFrame();  
+	    JOptionPane.showMessageDialog(f,"Hello, Let's Start the Game."); 
+		System.out.println("MAP Start");
+	    drawMap(xReal,yReal);
+	    if (maploadDone == 1) {
+	    	JOptionPane.showMessageDialog(f,"The game has started, now please pay attention to the map output from the console."); 
+	    }
+	    else {
+	    	JOptionPane.showMessageDialog(f,"ERROR!!! MAP cannot Load."); 
+	    }
+		System.out.println("\nMAP END");
+	    while (youWin == true) {
+	    	navigationPannel();
+	    	drawMap(xReal,yReal);
+	    }
+    	JOptionPane.showMessageDialog(f,"Congratulations! ! you win! !"); 
+	}
+	
+	public static int drawMap (int xReal, int yReal) {
+		int gridFinal = 5;
+		int totalLine = 5;
+		int haveDraw = 0;
+		System.out.print("|-------------|");
+		for (int a = 0;a < gridFinal-1;a++ ) {
+			System.out.print("-------------|");
+		}
+		for (int line = 0;line < totalLine;line++ ) {
+			for (int b = 0;b < gridFinal;b++ ) {
+				System.out.print("\n");
+				
+				if(b==2 & line==yReal-1 & xReal == 1) {
+					System.out.print("|You Are Here!|");
+					haveDraw = 1;
+				}
+				else {
+					System.out.print("|             |");
+				}
+
+				for (int c = 0;c < gridFinal-1;c++ ) {
+					if(b==2 & line==yReal-1 & haveDraw == 0 & c == xReal-2) {
+						System.out.print("You Are Here!|");
+					}
+					else {
+						System.out.print("             |");
+					}
+				}
+			}					
+			System.out.print("\n");
+			System.out.print("|-------------|");
+			for (int d = 0;d < gridFinal-1;d++ ) {
+				System.out.print("-------------|");
+			}		
+		}
+		return maploadDone = 1;
+	}
+	
+	public static void navigationPannel() {
+        Scanner input=new Scanner(System.in);
+        System.out.println("\nEnter 1 to North");
+        System.out.println("Enter 2 to South");
+        System.out.println("Enter 3 to West");
+        System.out.println("Enter 4 to West");
+        System.out.println("INPUT YOUR CHOOSE:");
+        int userInput = input.nextInt();
+        int navigation = userInput;
+        switch (navigation) {
+		case 1:
+			yReal--;
+			break;
+		case 2:
+			yReal++;
+			break;
+		case 3:
+			xReal--;
+			break;
+		case 4:
+			xReal++;
+			break;
+		}
+        input.close();
+		System.out.println("yReal=" + yReal);
+		System.out.println("xReal=" + xReal);
+	}
+	
 }
---
+++ src/zlahxqyl/button.java
@@ -0,0 +1,54 @@
+package zlahxqyl;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class button extends JPanel{
+    	
+	public button() {
+		setLayout(null);	//Set layout = null, make setBounds can use.
+		JButton up=new JButton("N");  
+        JButton down=new JButton("S");
+        JButton left=new JButton("W");
+        JButton right=new JButton("E");  
+        
+        up.setBounds(500,55,50,50);
+        down.setBounds(500,155,50,50);
+        left.setBounds(450,105,50,50);
+        right.setBounds(550,105,50,50);
+        
+        add(up);
+        add(down);
+        add(left);
+        add(right);
+        
+        up.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	new acquisitiontest().keytest(0);
+            }
+        });
+
+        down.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	new acquisitiontest().keytest(1);
+            }
+        });
+
+        left.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	new acquisitiontest().keytest(2);
+            }
+        });
+
+        right.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	new acquisitiontest().keytest(3);
+            }
+        });
+    }
+        
+	}
+	
+
