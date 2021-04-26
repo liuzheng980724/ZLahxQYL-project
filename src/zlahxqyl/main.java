c3c4285a393b9e34287f7d1cdec5a3504fa38e13
Leo <mail@liuzheng.org>
2021-04-26 12:16:27 +0800

    删除垃圾

--- src/zlahxqyl/main.java
+++
@@ -1,9 +0,0 @@
-package zlahxqyl;
-
-public class main {
-
-	public static void main (String[] args) {
-		mainlayout frame = new mainlayout();	//use CamelCase
-        frame.setVisible(true);
-	}
-}
